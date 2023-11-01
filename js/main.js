aca4fa85f45b4d7b8dea69b8e7ea718f5ede39b6
Caio Ikena <caioikena@gmail.com>
2023-10-31 21:49:10 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const perguntas = document.querySelectorAll('.pergunta-caixa');
+
+perguntas.forEach(pergunta => {
+    pergunta.addEventListener('click', (event) => {
+        if (pergunta.classList.contains('ativo')) {
+            pergunta.classList.remove('ativo');
+        } else {
+            perguntas.forEach(caixa => caixa.classList.remove('ativo'));
+            pergunta.classList.add('ativo');
+        }
+    });
+});
+
+window.addEventListener('resize', () => {
+    const imagem = document.querySelector('#imagem');
+    const detalhe = document.querySelector('#detalhe');
+    const largura = window.innerWidth;
+
+    if(largura >= 1440) {
+        imagem.src = '../images/illustration-woman-online-desktop.svg';
+        detalhe.src = '../images/bg-pattern-desktop.svg';
+    } else {
+        imagem.src = 'images/illustration-woman-online-mobile.svg';
+        detalhe.src = '../images/bg-pattern-mobile.svg';
+    }
+});
